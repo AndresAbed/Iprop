eb6be673d111a017ab5cfdcafb1abf942a7161d6
AndresAbed <abedandres@gmail.com>
2016-04-22 15:07:49 -0300

    Renames contact post path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'main#index'
-  post "main/contact_form", as: :contact
+  post "/" => "main#contact_form", as: :contact
 
   devise_for :admins, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
