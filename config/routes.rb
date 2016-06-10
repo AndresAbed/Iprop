e4f684c8e95a5fb3c0028377766dea3b262da757
AndresAbed <abedandres@gmail.com>
2016-06-10 17:11:41 -0300

    Adds property show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'properties/show'
+
   root 'main#index'
   get '/iprop-online' => 'main#iprop', as: :iprop
   get '/tasaciones' => 'appraisals#appraisals', as: :appraisals
   get '/empresa' => 'main#company', as: :company
   get '/consultoria' => 'main#consultancy', as: :consultancy
   get '/contacto' => 'main#contact', as: :contact
+
+  resources :properties, only: [:show], path: '/propiedades'
 
   post "/" => "main#contact_form"
   get  'appraisals/tasaciones', as: :appraisals_excel
