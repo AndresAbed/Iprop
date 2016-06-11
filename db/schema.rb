c6f148c5bd10e29db20c5c9c3973af4a4a38bb89
AndresAbed <abedandres@gmail.com>
2016-06-11 02:17:29 -0300

    Removes locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610225719) do
+ActiveRecord::Schema.define(version: 20160611051639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,14 +49,6 @@ ActiveRecord::Schema.define(version: 20160610225719) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "property_id"
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
