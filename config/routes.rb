997f7a2d26d3e3fb4278ae7d94037a77d8883a77
AndresAbed <abedandres@gmail.com>
2016-06-29 11:46:11 -0300

    Configures search on tags click

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,18 @@ Rails.application.routes.draw do
   get 'properties/show'
 
   root 'main#index'
-  get '/tasaciones' => 'appraisals#appraisals', as: :appraisals
-  post '/tasaciones' => 'appraisals#contact', as: :appraisals_contact
-  get '/empresa' => 'main#company', as: :company
-  get '/consultoria' => 'main#consultancy', as: :consultancy
-  get '/contacto' => 'main#contact'
-  post '/contacto' => 'main#contact_message', as: :contact
+  get '/tasaciones', to: 'appraisals#appraisals', as: :appraisals
+  post '/tasaciones', to: 'appraisals#contact', as: :appraisals_contact
+  get '/empresa', to: 'main#company', as: :company
+  get '/consultoria', to: 'main#consultancy', as: :consultancy
+  get '/contacto', to: 'main#contact'
+  post '/contacto', to: 'main#contact_message', as: :contact
 
   resources :properties, only: [:index, :show], path: '/propiedades'
+  post '/propiedades', to: 'properties#index'
   resources :posts, only: [:show], path: '/novedades'
 
-  get  'appraisals/tasaciones', as: :appraisals_excel
+  #get  'appraisals/tasaciones', as: :appraisals_excel
 
   devise_for :admins, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
