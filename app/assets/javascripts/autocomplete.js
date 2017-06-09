efa1d50241ce8268b84edce9276fe508d48104e4
Andres <abedandres@gmail.com>
2017-06-09 00:54:02 -0500

    Changes the pixels scripts
    Removes the extre head tags

--- app/assets/javascripts/autocomplete.js
+++
@@ -1,6 +0,0 @@
-function initialize() {
-  var input = document.getElementById('searchTextField');
-  var options = {componentRestrictions: {country: 'ar'}};
-  new google.maps.places.Autocomplete(input, options);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
